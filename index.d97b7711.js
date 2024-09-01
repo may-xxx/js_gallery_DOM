1d462cf9b29ab635d82db5265f03c83f2e8ba49f
May <miaqu138@gmail.com>
2024-09-01 18:52:29 +0200

    make build

---
+++ index.d97b7711.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.onclick=function(e){var l,n,a=e.target.closest("a");a&&(l=a.href,n=a.title,t.src=l,t.alt=n,e.preventDefault())};
+//# sourceMappingURL=index.d97b7711.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const thumbs = document.getElementById("thumbs");
+const largeImg = document.getElementById("largeImg");
+thumbs.onclick = function(evnt) {
+    const thumbnail = evnt.target.closest("a");
+    if (!thumbnail) return;
+    showThumbnail(thumbnail.href, thumbnail.title);
+    evnt.preventDefault();
+};
+function showThumbnail(href, title) {
+    largeImg.src = href;
+    largeImg.alt = title;
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
